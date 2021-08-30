ddd691aed0fb1eb6a811a85805296a818dd2f2b6
Holger Donath <holgerdonath@Holgers-Air.attlocal.net>
2021-08-30 16:35:55 -0500

    + selection methods

--- TP2/selection_methods.py
+++ TP2/selection_methods.py
@@ -1,5 +1,6 @@
 import random
 
+
 from Player import Player
 
 def random_selection(players: [Player], k: int) -> [Player]:
@@ -12,3 +13,49 @@ def random_selection(players: [Player], k: int) -> [Player]:
 
 def elite_selection(players: [Player], k: int) -> [Player]:
     raise NotImplemented()
+
+def roulette_selection(players: [Player], k: int) -> [Player]:
+    population_fitness = 0
+    r = 0
+    selection = []
+    player_accumulative_fitness = [(0, None)]
+    for player in players:
+        population_fitness = population_fitness + player.fitness
+    for player in players:
+        relative_fitness = player.fitnes / population_fitness
+        accumulative_fitness = accumulative_fitness + relative_fitness
+        player_accumulative_fitness.append(accumulative_fitness, player)
+    for i in range(k):
+        r = random.uniform(0, 1)
+        for j in range((len(player_accumulative_fitness)-1)):
+            lower_value = player_accumulative_fitness[j][0]
+            upper_value = player_accumulative_fitness[j+1][0]
+
+            if r in range(lower_value, upper_value):
+                selection.append(player_accumulative_fitness[j+1][1])
+    return selection
+
+def universal_selection(players: [Player], k: int) -> [Player]:
+    population_fitness = 0
+    r = 0
+    selection = []
+    player_accumulative_fitness = [(0, None)]
+    for player in players:
+        population_fitness = population_fitness + player.fitness
+    for player in players:
+        relative_fitness = player.fitnes / population_fitness
+        accumulative_fitness = accumulative_fitness + relative_fitness
+        player_accumulative_fitness.append(accumulative_fitness, player)
+    for i in range(k):
+        random_number = random.uniform(0, 1)
+        r = (random_number + i) / k
+        for j in range((len(player_accumulative_fitness) - 1)):
+            lower_value = player_accumulative_fitness[j][0]
+            upper_value = player_accumulative_fitness[j + 1][0]
+
+            if r in range(lower_value, upper_value):
+                selection.append(player_accumulative_fitness[j + 1][1])
+    return selection
+
+#retocar roulette y universal
+
